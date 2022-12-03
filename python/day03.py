d7ed2d747f2acc848b9b5dbaffd5e702959fe70b
Vinay Mehta <vinaymehta.nyc@gmail.com>
2022-12-03 14:57:57 -0800

    day03: part2

--- python/day03.py
+++ python/day03.py
@@ -8,7 +8,7 @@ def priority(char):
         return 1 + ord(char) - ord('a')
 
 with open(filename, 'r') as f:
-    data = f.readlines()
+    data = f.read().splitlines()
     priority_sum = 0
     for line in data:
         mid = len(line) // 2
@@ -17,3 +17,12 @@ with open(filename, 'r') as f:
 
 print(priority_sum)
 
+with open(filename, 'r') as f:
+    data = f.read().splitlines()
+    priority_sum = 0
+    data = [list(map(set, data[i:i+3])) for i in range(0,len(data),3)]
+    for group in data:
+        shared = group[0].intersection(group[1]).intersection(group[2]).pop()
+        priority_sum += priority(shared)
+
+print(priority_sum)
