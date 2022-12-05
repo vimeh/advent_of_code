7a5cdb17e5111d7c49e8d9492e721bd8db918c6e
Vinay Mehta <vinaymehta.nyc@gmail.com>
2022-12-05 14:24:10 -0800

    python, day05: part1

---
+++ python/day05.py
@@ -0,0 +1,33 @@
+filename = "data/05_i.txt"
+# filename = "data/05_e.txt"
+
+with open(filename, 'r') as f:
+    levels = []
+    line = f.readline()
+    num_stacks = 1 + (len(line) - 3)//4
+
+    while line[0] in [' ', '[']:
+        level = []
+        for i in range(num_stacks):
+            level.append(line[4*i:4*i+3])
+        levels.append(level)
+
+        line = f.readline()
+
+    levels = levels[:-1]
+    stacks = [[] for _ in range(num_stacks)]
+    for level in levels[::-1]:
+        for i, stack in enumerate(level):
+            if stack != '   ':
+                stacks[i].append(stack[1])
+
+    moves = f.read().splitlines()
+    moves = [move.split(' ') for move in moves]
+    moves = [list(map(int, [move[1], move[3], move[5]])) for move in moves]
+
+    for move in moves:
+        for _ in range(0, move[0]):
+            stacks[move[2]-1].append(stacks[move[1]-1].pop())
+
+    print(''.join([stack[-1] for stack in stacks]))
+
