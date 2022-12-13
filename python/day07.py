617b790874eecafef17d358e9f02b1ce8cbb13dd
Vinay Mehta <vinaymehta.nyc@gmail.com>
2022-12-12 16:13:41 -0800

    python, day07: part2

--- python/day07.py
+++ python/day07.py
@@ -1,7 +1,7 @@
 import unittest
 
 
-def sum_dir_lt(stream, limit):
+def dir_sizes(stream):
     stack = []
     res = {}
 
@@ -43,13 +43,31 @@ def sum_dir_lt(stream, limit):
     path = '/'.join([x[0] for x in stack]) + '/' + cwd
     res[path] = usize
 
+    return res
+
+
+def sum_dir_lt(stream, limit):
+    res = dir_sizes(stream)
     res = [(k, v) for k, v in res.items() if v < limit]
 
     return sum([x[1] for x in res])
 
 
+def min_delete(stream, tot, need):
+    res = dir_sizes(stream)
+    res = sorted(res.items(), key=lambda x: x[1])
+
+    free = tot - res[-1][1]
+    need -= free
+    for _, v in res:
+        if v > need:
+            return v
+
+
 class Tests(unittest.TestCase):
     limit = 100000
+    need = 30000000
+    tot = 70000000
 
     def setUp(self) -> None:
         filename = "data/07_e.txt"
@@ -67,6 +85,10 @@ class Tests(unittest.TestCase):
     def test_part1(self):
         res = 1428881
         self.assertEqual(sum_dir_lt(self.data, self.limit), res)
+
+    def test_part2(self):
+        res = 10475598
+        self.assertEqual(min_delete(self.data, self.tot, self.need), res)
 
 
 if __name__ == '__main__':
