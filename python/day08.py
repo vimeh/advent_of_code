acb581811ecba4f23974e8f21f90cf3a498cb0eb
Vinay Mehta <vinaymehta.nyc@gmail.com>
2022-12-14 11:41:21 -0800

    python, day08: remove numpy

--- python/day08.py
+++ python/day08.py
@@ -1,44 +1,43 @@
 import unittest
-import numpy as np
+from math import prod
 
 
 def get_visible_trees(grid):
-    M, N = grid.shape
-
+    M, N = len(grid), len(grid[0])
     # edge trees always visible
     visible = 2 * M + 2 * (N - 2)
 
-    for n in range(1, N - 1):
-        for m in range(1, M - 1):
-            vis_up = all(grid[m][n] > x for x in grid[:m, n])
-            vis_dn = all(grid[m][n] > x for x in grid[m + 1:, n])
-            vis_lt = all(grid[m][n] > x for x in grid[m, :n])
-            vis_rt = all(grid[m][n] > x for x in grid[m, n + 1:])
+    grid_t = [*zip(*grid)]
 
-            if vis_up or vis_dn or vis_lt or vis_rt:
-                visible += 1
+    for m in range(1, M - 1):
+        row = grid[m]
+        for n in range(1, N - 1):
+            col = grid_t[n]
+            val = grid[m][n]
+
+            runs = (row[n - 1::-1], row[n + 1:], col[m - 1::-1], col[m + 1:])
+            visible += any(all(x < val for x in run) for run in runs)
 
     return visible
 
 
 def get_max_score(grid):
-    M, N = grid.shape
+    M, N = len(grid), len(grid[0])
 
-    def num_trees(val, line):
-        line = np.atleast_1d(line)
-        for i, x in np.ndenumerate(line):
-            if x >= val:
-                return i[0] + 1
-
-        return line.size
+    grid_t = [*zip(*grid)]
 
     max_score = 0
     for m in range(1, M - 1):
+        row = grid[m]
         for n in range(1, N - 1):
-            val = grid[m, n]
-            score = num_trees(val, np.flip(grid[:m, n])) * num_trees(
-                val, grid[m + 1:, n]) * num_trees(val, np.flip(
-                    grid[m, :n])) * num_trees(val, grid[m, n + 1:])
+            col = grid_t[n]
+            val = grid[m][n]
+
+            runs = (row[n - 1::-1], row[n + 1:], col[m - 1::-1], col[m + 1:])
+
+            score = prod(
+                next((k + 1 for k, t in enumerate(run) if t >= val), len(run))
+                for run in runs)
             max_score = max(max_score, score)
 
     return max_score
@@ -51,19 +50,13 @@ class Tests(unittest.TestCase):
         with open(filename) as f:
             data = f.read().splitlines()
 
-        for i in range(len(data)):
-            data[i] = [int(x) for x in data[i]]
-
-        self.data_example = np.array(data)
+        self.data_example = [[int(x) for x in line] for line in data]
 
         filename = 'data/08_i.txt'
         with open(filename) as f:
             data = f.read().splitlines()
 
-        for i in range(len(data)):
-            data[i] = [int(x) for x in data[i]]
-
-        self.data = np.array(data)
+        self.data = [[int(x) for x in line] for line in data]
 
     def test_part1_example(self):
         self.assertEqual(21, get_visible_trees(self.data_example))
@@ -75,7 +68,7 @@ class Tests(unittest.TestCase):
         self.assertEqual(8, get_max_score(self.data_example))
 
     def test_part2(self):
-        self.assertEqual(8, get_max_score(self.data))
+        self.assertEqual(496650, get_max_score(self.data))
 
 
 if __name__ == '__main__':
