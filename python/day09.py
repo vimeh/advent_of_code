5c6788d24c676e2222897a172991a8d67f3a23b4
Vinay Mehta <vinaymehta.nyc@gmail.com>
2022-12-14 18:13:20 -0800

    python, day09: part2

--- python/day09.py
+++ python/day09.py
@@ -1,22 +1,79 @@
 import unittest
 
+DEBUG = False
 
-def pos_visited(data):
+
+def pos_visited(data, len_tail):
     visited = set()
-    start = (0, 0)
-    head = (0, 0)
-    tail = (0, 0)
-    visited.add(tail)
+    rope = [(0, 0)] * (1 + len_tail)
+    visited.add(rope[-1])
     moves = {'R': (0, 1), 'L': (0, -1), 'U': (1, 0), 'D': (-1, 0)}
 
     for move in data:
         d, s = move[0], int(move[1])
-        for _ in range(s):
-            temp = head
-            head = (head[0] + moves[d][0], head[1] + moves[d][1])
-            if any(abs(h - t) > 1 for h, t in zip(head, tail)):
-                tail = temp
-                visited.add(tail)
+        for j in range(s):
+            # move the head
+            diff = moves[d]
+            prev = rope[0]
+            rope[0] = tuple(x + y for x, y in zip(prev, diff))
+
+            # check the following nodes
+            for i in range(1, len(rope)):
+                if any(abs(p - n) > 1 for p, n in zip(rope[i - 1], rope[i])):
+                    if sum(abs(p - n)
+                           for p, n in zip(rope[i - 1], rope[i])) == 3:
+                        if diff in moves.values():
+                            prev, rope[i] = rope[i], prev
+                        else:
+                            prev, rope[i] = rope[i], tuple(
+                                x + y for x, y in zip(rope[i], diff))
+                    else:
+                        direction = tuple(
+                            (x - y) // 2 for x, y in zip(rope[i - 1], rope[i]))
+                        prev, rope[i] = rope[i], tuple(
+                            x + y for x, y in zip(rope[i], direction))
+                        # prev, rope[i] = rope[i], tuple(
+                        # x + y for x, y in zip(rope[i], moves[d]))
+
+                diff = tuple(x - y for x, y in zip(rope[i], prev))
+            if (DEBUG) and move == ['L', '8']:
+                print(f'== {move} == {j}')
+                for i in range(-14, 14):
+                    for j in range(-14, 14):
+                        if (-i, j) in rope:
+                            print(f'{rope.index((-i,j))}', end='')
+                        elif (i, j) == (0, 0):
+                            print('s', end='')
+                        else:
+                            print('.', end='')
+                    print('')
+
+            # at end of move, add tail to visited
+            visited.add(rope[-1])
+
+        if (DEBUG):
+            print(f'== {move} ==')
+            for i in range(-14, 14):
+                for j in range(-14, 14):
+                    if (-i, j) in rope:
+                        print(f'{rope.index((-i,j))}', end='')
+                    elif (i, j) == (0, 0):
+                        print('s', end='')
+                    else:
+                        print('.', end='')
+                print('')
+
+    if (DEBUG):
+        print('VISITED')
+        for i in range(-14, 14):
+            for j in range(-14, 14):
+                if (-i, j) in visited:
+                    print('#', end='')
+                elif (i, j) == (0, 0):
+                    print('s', end='')
+                else:
+                    print('.', end='')
+            print('')
 
     return len(visited)
 
@@ -38,11 +95,27 @@ class Solution(unittest.TestCase):
         data = [line.split() for line in data]
         self.data = data
 
+        filename = 'data/09_e2.txt'
+        with open(filename, 'r') as f:
+            data = f.read().splitlines()
+
+        data = [line.split() for line in data]
+        self.data_example2 = data
+
     def test_part1_example(self):
-        self.assertEqual(13, pos_visited(self.data_example))
+        self.assertEqual(13, pos_visited(self.data_example, 1))
 
     def test_part1(self):
-        self.assertEqual(6087, pos_visited(self.data))
+        self.assertEqual(6087, pos_visited(self.data, 1))
+
+    def test_part2_example1(self):
+        self.assertEqual(1, pos_visited(self.data_example, 9))
+
+    def test_part2_example2(self):
+        self.assertEqual(36, pos_visited(self.data_example2, 9))
+
+    def test_part2(self):
+        self.assertEqual(2493, pos_visited(self.data, 9))
 
 
 if __name__ == '__main__':
