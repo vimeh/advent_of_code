eb8e247e846c363ae67534fecec477aefc259d9d
Vinay Mehta <vinaymehta.nyc@gmail.com>
2022-12-19 11:41:16 -0800

    add python template

---
+++ python/template.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class Solution(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_part1_example(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
