95b8d43e2b94dbf49fb7c344ba31f5339db9394f
Vinay Mehta <vinaymehta.nyc@gmail.com>
2022-12-19 15:26:45 -0800

    python: day11, part2

--- python/day11.py
+++ python/day11.py
@@ -1,5 +1,6 @@
 import unittest
 import re
+from math import prod
 
 
 class Monkey():
@@ -21,9 +22,11 @@ class Monkey():
         self.div = lambda x: test_t if x % test == 0 else test_f
 
 
-def monkey_business(data, rounds):
+def monkey_business(data, rounds, worry):
     len_monkey = 7
     monkeys = []
+    modulos = []
+
     for monkey in [
             data[s:s + len_monkey] for s in range(0, len(data), len_monkey)
     ]:
@@ -33,18 +36,23 @@ def monkey_business(data, rounds):
         op, co = re.search(r'([+*]) (\d+|old)', monkey[2]).groups()
 
         test = int(re.search(r'\d+', monkey[3]).group(0))
+        modulos.append(test)
         test_t = int(re.search(r'\d+', monkey[4]).group(0))
         test_f = int(re.search(r'\d+', monkey[5]).group(0))
         monkeys.append(Monkey(items, op, co, test, test_t, test_f))
 
+    supermodulo = prod(modulos)
+
     for _ in range(rounds):
         for monkey in monkeys:
-            for _ in range(len(monkey.items)):
+            i = len(monkey.items)
+            monkey.inspects += i
+            for _ in range(i):
                 item = monkey.items.pop(0)
-                item = monkey.op(item) // 3
+                item = monkey.op(item) // worry
                 to = monkey.div(item)
+                item = item % supermodulo
                 monkeys[to].items.append(item)
-                monkey.inspects += 1
 
     monkeys = sorted(monkeys, key=lambda x: x.inspects, reverse=True)
 
@@ -66,10 +74,17 @@ class Solution(unittest.TestCase):
         self.data = data
 
     def test_part1_example(self):
-        self.assertEqual(10605, monkey_business(self.data_example, 20))
+        self.assertEqual(10605, monkey_business(self.data_example, 20, 3))
 
     def test_part1(self):
-        self.assertEqual(54036, monkey_business(self.data, 20))
+        self.assertEqual(54036, monkey_business(self.data, 20, 3))
+
+    def test_part2_example(self):
+        self.assertEqual(2713310158,
+                         monkey_business(self.data_example, 10000, 1))
+
+    def test_part2(self):
+        self.assertEqual(13237873355, monkey_business(self.data, 10000, 1))
 
 
 if __name__ == '__main__':
