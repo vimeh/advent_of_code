77d991bae9385d5b61a5e5729d89d479baca9c0b
Vinay Mehta <vinaymehta.nyc@gmail.com>
2022-12-21 15:20:18 -0800

    python: day13, part1 yet another working example (not input), but now even more readable

--- python/day13.py
+++ python/day13.py
@@ -9,7 +9,8 @@ def is_correct_order(lt, rt):
     if type(rt) == int:
         return is_correct_order(lt, [rt])
 
-    # compare lens
+    # this is the problem here
+    # not always the case that when len(lt) == 0 you're good. might just continue
     if len(lt) == 0:
         return True
     elif len(rt) == 0:
@@ -17,26 +18,24 @@ def is_correct_order(lt, rt):
 
     # anywhere where type(lt[0]) or type(rt[0]) is a list,
     # recurse on rest of list and do a cast on xt[0] if necessary
-    if type(lt[0]) == list:
-        if type(rt[0]) == list:
-            return is_correct_order(lt[0], rt[0]) and is_correct_order(
-                lt[1:], rt[1:])
+    types = (type(lt[0]), type(rt[0]))
+    if types == (list, list):
+        return is_correct_order(lt[0], rt[0]) and is_correct_order(
+            lt[1:], rt[1:])
+    elif types == (list, int):
+        return is_correct_order(lt[0], [rt[0]]) and is_correct_order(
+            lt[1:], rt[1:])
+    elif types == (int, list):
+        return is_correct_order([lt[0]], rt[0]) and is_correct_order(
+            lt[1:], rt[1:])
+    elif types == (int, int):
+        # both are ints and we can finally iterate
+        if lt[0] < rt[0]:
+            return True
+        elif lt[0] == rt[0]:
+            return is_correct_order(lt[1:], rt[1:])
         else:
-            return is_correct_order(lt[0], [rt[0]]) and is_correct_order(
-                lt[1:], rt[1:])
-    elif type(lt[0]) == int:
-        if type(rt[0]) == list:
-            return is_correct_order([lt[0]], rt[0]) and is_correct_order(
-                lt[1:], rt[1:])
-        else:
-            # both are ints and we can finally iterate
-            print(f"comparing {lt[0]} and {rt[0]}")
-            if lt[0] < rt[0]:
-                return True
-            elif lt[0] == rt[0]:
-                return is_correct_order(lt[1:], rt[1:])
-            else:
-                return False
+            return False
 
     print(f'INVALID STATE: lt: {type(lt)}, rt: {type(rt)}')
     print(f'\tlt: {lt}, rt: {rt}')
@@ -47,7 +46,6 @@ def sum_correct_order(data):
     offset = 3
     for idx, pair in enumerate(
         [data[s:s + offset] for s in range(0, len(data), offset)]):
-        print('')
         if is_correct_order(eval(pair[0]), eval(pair[1])):
             sum_idx += idx + 1
 
